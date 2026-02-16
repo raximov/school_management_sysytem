4fa36c18aa0dd692ad481d4279ce2650464c32ed
abduhamid.raximov <abduhamid.raximov@uzinfocom.uz>
2026-02-16 12:02:08 +0500

    removingtemplates html"

--- school_project/testapp/templatetags/sum_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def total_score(queryset, field):
-    return round(sum(getattr(obj, field, 0) for obj in queryset), 2)
